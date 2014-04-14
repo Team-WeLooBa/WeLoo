e0c47cc8a27038dd66c52f49f01799cc7e2efdc6
slovey528 <jiangyumao@gmail.com>
2014-04-13 21:34:24 -0400

    indent change, and reset the rule pattern to handle white spaces

--- rules/eatWhat/eat_what.js
+++ rules/eatWhat/eat_what.js
@@ -6,24 +6,23 @@ var myFriend = ", 我的朋友";
 
 module.exports = function(webot){
   webot.set('random restaurant',{
-  description:'Random: 随机查询餐馆',
-  pattern: /(吃啥)/,
-  handler: function(info,next){
-    info = utils.sanitizeInfo(info);
-    var database = mongo.connect(mongoUri,collecions,function(err, db){
-         db.collection("restaurant",function(err,collection){
-          // collection.find().toArray(function(err,cb){
-          //   console.log(cb);
-          // });
+    description:'Random: 随机查询餐馆',
+    pattern: /^(吃啥)\s*/,
+    handler: function(info,next){
+      info = utils.sanitizeInfo(info);
+      var database = mongo.connect(mongoUri,collecions,function(err, db){
+        db.collection("restaurant",function(err,collection){
           collection.count(function(err,result){
             var num = Math.floor((Math.random()*(result)));
             collection.find({index:num}).toArray(function(err,callback){
-               var name = callback[0]['name'];
-               var replyText = utils.dynamicReplyArray[Math.floor(Math.random()*((utils.dynamicReplyArray.length)))];
-               // console.log(utils.dynamicReplyArray.length);
-               replyText = replyText + name + myFriend;
-               next(null,replyText);
-                });});
-          
-        })})}});
+              var name = callback[0]['name'];
+              var replyText = utils.dynamicReplyArray[Math.floor(Math.random()*((utils.dynamicReplyArray.length)))];
+              replyText = replyText + name + myFriend;
+              next(null,replyText);
+            });
+          });    
+        })
+      })
+    }
+  });
 }
